3587a44295a4f5833163dbe408ab01bdff6ecbc9
Sam Brown <jumpbro@hotmail.com>
2022-04-19 19:10:49 +1000

    initial setup

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/* 
+ROCK PAPER SCISSORS
+
+To Implement:
+
+ui:
+    3 image/icons for rock/paper/scissors
+    tally counter to see win/loss ration
+    get from fontawesome (hand-scissors, hand-fist, hand  )
+
+features:    
+    user clicks one icon to choose hand
+
+    generate random selection for ai (try implement sound?)
+
+    show rock image shake 3 times then display user and ai selection (can use transform(scale) to increase size momentarily)
+
+    some kind of animation to show if win or lose
+    add to tally counter, make winner flash green and loser flash red? 
+
+*/
